e4b3adfd31b3d66daee9ae1958a7ed9347e0cb3c
Alejo30192 <104231499+Alejo30192@users.noreply.github.com>
2022-11-02 22:23:29 -0500

    Delete siocaweb directory

--- siocaweb/src/main/java/com/proyecto/sioca/repository/IUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.proyecto.sioca.repository;
-
-import com.proyecto.sioca.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserRepository extends JpaRepository <User,Long>{
-
-}
